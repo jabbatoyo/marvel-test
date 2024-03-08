9019413f6f00cca4b46c829705bfe867aa27be81
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-08 21:40:53 +0100

    add detail loader

--- src/routes/loaders.ts
+++ src/routes/loaders.ts
@@ -1,9 +1,18 @@
 import { LoaderFunction } from "react-router-dom";
-import { getCharacters } from "../api/endPoints";
+import { getCharacter, getCharacters } from "../api/endPoints";
 
 export const LoaderCharacters: LoaderFunction = async ({ request }) => {
   const url = new URL(request.url);
   const searchTerm = url.searchParams.get("search");
   const res = await getCharacters(searchTerm ?? "");
+  return res;
+};
+
+export const LoaderDetailCharacterAndCommics: LoaderFunction = async ({
+  params,
+}) => {
+  const { id } = params;
+  const res = await getCharacter(Number(id));
+
   return res;
 };
