f1aa3b1e962b784b01dce4831bece90dd48008e4
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-09 14:43:26 +0100

    update component heartIcon

--- src/components/Icons/HeartIcon.tsx
+++ src/components/Icons/HeartIcon.tsx
@@ -4,15 +4,16 @@ export type HeartProp = {
   isChecked: boolean;
 };
 const HeartIcon = (props: HeartProp) => {
+  const { color, isChecked, ...rest } = props;
   return (
     <svg
       className="heart-icon"
       style={{ cursor: "pointer", width: "24px", height: "24px" }}
-      {...props}
+      {...rest}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      stroke={props.color}
+      stroke={color}
     >
       <g id="SVGRepo_bgCarrier" strokeWidth="0" />
       <g
@@ -23,8 +24,8 @@ const HeartIcon = (props: HeartProp) => {
       <g id="SVGRepo_iconCarrier">
         <path
           d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z"
-          fill={props.isChecked ? props.color : ""}
-          stroke={props.color}
+          fill={isChecked ? color : ""}
+          stroke={color}
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
