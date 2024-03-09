742f5851649f9d27414112c1627e7db58be33561
Juan Obiang <juanobiang@MacBook-Pro-de-Juan.local>
2024-03-09 14:33:34 +0100

    add new const for color of the styles

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -2,3 +2,7 @@ export const API_BASE_URL = import.meta.env.VITE_APP_BASE_API_URL;
 export const API_PUBLIC_KEY = `?ts=1&apikey=${
   import.meta.env.VITE_APP_PUBLIC_API_KEY
 }&hash=${import.meta.env.VITE_APP_HASH_API}`;
+
+export const colorRed = "#ec1e24";
+export const colorWhite = "#fff";
+export const colorBlack = "#000";
